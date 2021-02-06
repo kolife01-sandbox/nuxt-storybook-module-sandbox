174f510224112f85fec6385758090b6b0b8b8a9e
Kohei Tanaka <kohei69@gmail.com>
2021-02-06 23:07:50 +0900

    chore : addon追加 eslint fix 実行

--- components/Link.stories.js
+++ components/Link.stories.js
@@ -1,5 +1,5 @@
 export default {
-  title: 'Link'
+  title: 'Link',
 }
 
 export const NuxtWebsite = () => '<Link />'
--- nuxt.config.js
+++ nuxt.config.js
@@ -43,5 +43,7 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 
-  storybook: {}
+  storybook: {
+    addons: ['@storybook/addon-controls', '@storybook/addon-notes'],
+  },
 }
